546e1884809b131303a85a4f69509b5147b72fed
Tei Roberts <ten3roberts@gmail.com>
2023-03-10 16:47:26 +0100

    chore: fmt

--- lua/graphene.lua
+++ lua/graphene.lua
@@ -7,69 +7,70 @@ local Context = require("graphene.context")
 
 local a = require("plenary.async")
 function M.init(dir)
-  local cur_file = fn.expand("%:p:t")
+	local cur_file = fn.expand("%:p:t")
 
-  if not dir then
-    if vim.bo.buftype == "" then
-      dir = fn.expand("%:p:h")
-    else
-      dir = fn.getcwd()
-    end
-  end
+	if not dir then
+		if vim.bo.buftype == "" then
+			dir = fn.expand("%:p:h")
+		else
+			dir = fn.getcwd()
+		end
+	end
 
-  a.run(function()
-    local ctx = Context.new(dir)
-    a.util.scheduler()
-    api.nvim_win_set_buf(0, ctx.bufnr)
-    ctx:display()
+	a.run(function()
+		local ctx = Context.new(dir)
+		a.util.scheduler()
+		api.nvim_win_set_buf(0, ctx.bufnr)
+		ctx:display()
 
-    M.setup_mappings(ctx)
+		M.setup_mappings(ctx)
 
-    ctx:focus(cur_file)
-  end)
+		ctx:focus(cur_file)
+	end)
 end
 
 function M.setup_mappings(ctx)
-  local function map(l, r)
-    vim.keymap.set({ "n", "v" }, l, r, { buffer = ctx.bufnr, nowait = true })
-  end
+	local function map(l, r)
+		vim.keymap.set({ "n", "v" }, l, r, { buffer = ctx.bufnr, nowait = true })
+	end
 
-  for k, v in pairs(config.mappings) do
-    map(
-      k,
-      a.void(function()
-        v(ctx)
-      end)
-    )
-  end
+	for k, v in pairs(config.mappings) do
+		map(
+			k,
+			a.void(function()
+				v(ctx)
+			end)
+		)
+	end
 end
 
 ---@param opts graphene.config
 function M.setup(opts)
-  config.setup(opts)
+	config.setup(opts)
 
-  local group = api.nvim_create_augroup("FileExplorer", { clear = true })
-  local function au(event, o)
-    opts.group = group
-    api.nvim_create_autocmd(event, o)
-  end
+	local group = api.nvim_create_augroup("FileExplorer", { clear = true })
+	local function au(event, o)
+		opts.group = group
+		api.nvim_create_autocmd(event, o)
+	end
 
-  if config.override_netrw then
-    -- vim.g.loaded_netrw = 1
-    -- vim.g.loaded_netrwPlugin = 1
-    au({ "BufEnter" }, {
-      callback = function(o)
-        if vim.fn.isdirectory(o.file) == 1 then
-          M.init(o.file)
-        end
-      end,
-    })
-  end
+	if config.override_netrw then
+		-- vim.g.loaded_netrw = 1
+		-- vim.g.loaded_netrwPlugin = 1
+		au({ "BufEnter" }, {
+			callback = function(o)
+				if vim.fn.isdirectory(o.file) == 1 and api.nvim_buf_get_option(o.buf, "buftype") == "" then
+					vim.notify(string.format("%s is a directory", vim.inspect(o)))
+					M.init(o.file)
+				end
+			end,
+		})
+	end
 
-  api.nvim_create_user_command("Graphene", function(o)
-    local args = o.args ~= "" and o.args
-    require("graphene").init(args)
-  end, { nargs = "?", desc = "Open graphene file browser", complete = "file" })
+	api.nvim_create_user_command("Graphene", function(o)
+		local args = o.args ~= "" and o.args
+		require("graphene").init(args)
+	end, { nargs = "?", desc = "Open graphene file browser", complete = "file" })
 end
 
 ---@class StatuslineOpts
@@ -79,13 +80,13 @@ end
 ---@param opts StatuslineOpts
 ---@return function
 function M.make_statusline(opts)
-  opts = opts or {}
-  local icon = opts.icon ~= false
-  local hl = opts.hl ~= false
-  local sl = require("graphene.statusline").statusline
-  return function()
-    return sl(icon, hl)
-  end
+	opts = opts or {}
+	local icon = opts.icon ~= false
+	local hl = opts.hl ~= false
+	local sl = require("graphene.statusline").statusline
+	return function()
+		return sl(icon, hl)
+	end
 end
 
 return M
